3328b37904178a0da9aedf7d7080511a3322d6b0
P27389 <andreas.mueller@fh-hagenberg.at>
2024-12-12 12:26:19 +0100

    ue05

---
+++ src/ue05/vue-01/src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import DataBindings from "./components/DataBindings.vue";
+</script>
+
+<template>
+  <h1>01 Data Bindings</h1>
+  <DataBindings></DataBindings>
+  <hr>
+</template>
+
+<style scoped>
+
+</style>
